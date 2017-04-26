0ca6162a337263ef24ae3990b8ff82e4c4ed4e5b
Bart Veneman <bartveneman@gmail.com>
2017-04-26 22:17:52 +0200

    Add jokes

---
+++ data/css-jokes.js
@@ -0,0 +1,9 @@
+module.exports = [
+  "#titanic {\n\tfloat:none;\n}",
+  ".sniper-mode-engaged {\n\tcursor: crosshair;\n}",
+  "#big-bang::before {\n\tcontent: \"\";\n}",
+  "#chucknorris {\n\tcolor: #BADA55;\n}",
+  ".yomama {\n\twidth: 99999999px;\n}",
+  "#lego {\n\tdisplay: block;\n}",
+  "#ikea {\n\tdisplay: table;\n}"
+]
