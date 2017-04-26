68323d3dc46bc659e2a30adc966500e540cfa778
Bart Veneman <bartveneman@gmail.com>
2017-04-26 20:13:48 +0200

    bot

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const Bot = require('slackbots')
+const settings = {
+  token: 'xoxb-175055758964-fbB80fO6tVUccFPc6N1El2JN',
+  name: 'CSS Facts'
+}
+
+const bot = new Bot(settings)
+
+bot.on('start', () => {
+  bot.postMessageToChannel('random', 'Hello!!!')
+})
