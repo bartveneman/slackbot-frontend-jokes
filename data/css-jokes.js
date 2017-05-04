c7eb0ce8fac71cf2c89d9248984d7ace1081bbe4
Tim Huijzers <Tim.huijzers@drukwerkdeal.nl>
2017-05-04 14:49:50 +0200

    extended wife

--- data/css-jokes.js
+++ data/css-jokes.js
@@ -6,6 +6,6 @@ module.exports = [
   ".yomama {\n\twidth: 99999999px;\n}",
   "#lego {\n\tdisplay: block;\n}",
   "#ikea {\n\tdisplay: table;\n}",
-  "#my-wife {\n\tright: 100%;\n}",
+  "#my-wife {\n\tright: 100%;margin:0%;\n}",
   ".wilders {\n\talign: right;\n}"
 ]
