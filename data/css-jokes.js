2a3f7b3b093278734b7287bcf4a627642c942adb
Bart Veneman <bartveneman@users.noreply.github.com>
2017-05-04 20:17:41 +0200

    Table layout joke (#6)

--- data/css-jokes.js
+++ data/css-jokes.js
@@ -9,5 +9,6 @@ module.exports = [
   "#my-wife {\n\tright: 100%;\n}",
   ".wilders {\n\talign: right;\n}",
   ".delorean {\n\tz-index: -1955;\n}",
-  ".red-door {\n\tcolor: black;\n}"
+  ".red-door {\n\tcolor: black;\n}",
+  "Why did the web developer leave the restaurant?\n\tBecause of the table layout.",
 ]
